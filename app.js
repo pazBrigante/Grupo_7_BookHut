6f35476179aa053764946016bc2262d139cc29af
sergiolaulhe <sergiolaulhe@hotmail.com>
2022-06-20 13:15:59 -0300

    actualizacion

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
+const publicFolderPath = path.resolve(__dirname, './public');
+app.use(express.static(publicFolderPath));
 
 app.listen(3000, () => console.log('Servidor en linea en puerto 3000'));
 
 app.get('/', function (req, res){
-    res.send('Bienvenidos a The BooK Hut');
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
