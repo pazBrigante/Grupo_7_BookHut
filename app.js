a86195a24c0eb17ee4368e16073a2e6a5a40d1a6
sergiolaulhe <sergiolaulhe@hotmail.com>
2022-06-20 18:38:40 -0300

    actualizacion de contenido y wireframes

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 const express = require('express');
-<<<<<<< HEAD
-
-const app = express();
-
-app.listen(3000, () => console.log('Esto fue exitoso'));
-
-app.get('/', function (req, res){
-    res.send('Bienvenidos a BooK Hut');
-=======
 const app = express();
 
 const path = require('path');
@@ -18,5 +9,4 @@ app.listen(3000, () => console.log('Servidor en linea en puerto 3000'));
 
 app.get('/', function (req, res){
     res.sendFile(path.resolve(__dirname, './views/index.html'));
->>>>>>> 6f35476179aa053764946016bc2262d139cc29af
 });
