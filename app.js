f60fd7f7852f38c58191d8f8f98be78d1c93f70e
Lea102 <lscquizzato@gmail.com>
2022-06-27 16:05:23 -0300

    Trabajo en el Header

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const publicFolderPath = path.resolve(__dirname, './public');
 app.use(express.static(publicFolderPath));
 
-app.listen(3000, () => console.log('Servidor en linea en puerto 3000'));
+app.listen(3030, () => console.log('Servidor en linea en puerto 3030'));
 
 app.get('/', function (req, res){
     res.sendFile(path.resolve(__dirname, './views/index.html'));
