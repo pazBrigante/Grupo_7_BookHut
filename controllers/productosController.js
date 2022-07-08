aaaa17d9fa4bff4ee1668075df41563695ec2164
Lea102 <lscquizzato@gmail.com>
2022-07-08 11:26:20 -0300

    Add Controller ajuste menor

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const controlador = {
     index: function (req, res){
-        console.log(__dirname);
+       
         res.sendFile(path.resolve(__dirname, '../views/index.html'));
         
     },
