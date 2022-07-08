75eec7ac8c2ce167446f8d8a56684d7dbea81110
Lea102 <lscquizzato@gmail.com>
2022-07-08 14:30:40 -0300

    Add Archivos de Rutas

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path');
 const publicFolderPath = path.resolve(__dirname, './public');
 const mainController = require("./controllers/mainController.js");
-const rutasMain = require("./routes/main");
+const rutasMain = require("./routes/mainRoutes");
 
 
 
@@ -14,7 +14,7 @@ app.listen(3030, () => console.log('Servidor en linea en puerto 3030'));
 
 
 
-app.get('/',mainController.index );
+app.use('/',rutasMain );
 
-app.get("/register",mainController.registro);
+
 
