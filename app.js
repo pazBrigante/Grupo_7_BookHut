f5b9da3cd669c725bf1e3ab0ca4c28add9485c68
Lea102 <lscquizzato@gmail.com>
2022-07-08 14:54:30 -0300

    Detalles Routes

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const app = express();
 
 const path = require('path');
 const publicFolderPath = path.resolve(__dirname, './public');
+
 const mainController = require("./controllers/mainController.js");
 const rutasMain = require("./routes/mainRoutes");
-
-
 
 app.use(express.static(publicFolderPath));
 
 app.listen(3030, () => console.log('Servidor en linea en puerto 3030'));
-
-
 
 app.use('/',rutasMain );
 
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-
 const path = require('path');
 const publicFolderPath = path.resolve(__dirname, './public');
-const productosController = require("../controllers/mainController.js");
 
-
+const mainController = require("../controllers/mainController.js");
 
 router.use(express.static(publicFolderPath)); 
 
 
-
-router.get('/',productosController.index );
-
-router.get("/register",productosController.registro);
+router.get('/',mainController.index );
+router.get("/register",mainController.registro);
 
 module.exports =router;
