cdc9c44740ad84a473c4eb5023f155555cf8dcff
Lea102 <lscquizzato@gmail.com>
2022-10-03 15:20:59 -0300

    Acomodé Validation y saqué grupo de register

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -18,14 +18,7 @@ var storage = multer.diskStorage({
 
 var upload = multer({
     storage: storage
-    //  , 
-    // fileFilter: (req, file, cb) => {
-    //    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-    //     cb(null, true);
-    //  } else {
-    //    cb(null, false);
-    //   return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-    // }}
+   
 });
 
 // ************ Controller Require ************
@@ -33,45 +26,45 @@ const productsController = require('../controllers/productsController');
 const carritoController = require('../controllers/carritoController');
 // ************ Controller Require ************
 
+const checkEditProduct =[   check("nombre").isLength({ min: 5 }).withMessage("Nombre Mínimo 5 Caracteres"),
+check("codigo").isLength({ min: 4 }).withMessage("Código Mínimo 4 Caracteres"),
+check("autor").isLength({ min: 4 }).withMessage("Autor Mínimo 4 Caracteres"),
+check("descripcion").isLength({ min: 4 }).withMessage("Autor Mínimo 4 Caracteres"),
+check("precio").isNumeric({ min: 2 }).withMessage("Precio Inválido"),
+check("categoria").custom(async (categoria, { req }) => {
+    console.log("categoria", categoria);
+    console.log("categoria", "seleccionados");
+    if ((categoria != "lanzamientos") && (categoria != "seleccionados") && (categoria != "masVendidos")) {
+        throw new Error('Categoría incorrecta')
+    }
+})
+];
 
-
-
+const checkCreateProduct=[check("nombre").isLength({ min: 5 }).withMessage("Nombre Mínimo 5 Caracteres"),
+check("codigo").isLength({ min: 4 }).withMessage("Código Mínimo 4 Caracteres"),
+check("autor").isLength({ min: 4 }).withMessage("Autor Mínimo 4 Caracteres"),
+check("descripcion").isLength({ min: 4 }).withMessage("Descripción Mínimo 4 Caracteres"),
+check("precio").isNumeric({ min: 2 }).withMessage("Precio Inválido"),
+check("categoria").custom(async (categoria, { req }) => {
+    console.log("categoria", categoria);
+    if ((categoria != "lanzamientos") && (categoria != "seleccionados") && (categoria != "masVendidos")) {
+        throw new Error('Categoría incorrecta')
+    }
+}),
+];
 
 /*** CREATE ONE PRODUCT ***/
 router.get('/create', productsController.create);
+
+
 router.post('/', upload.single("product-img"),
-    [check("nombre").isLength({ min: 5 }).withMessage("Nombre Mínimo 5 Caracteres"),
-    check("codigo").isLength({ min: 4 }).withMessage("Código Mínimo 4 Caracteres"),
-    check("autor").isLength({ min: 4 }).withMessage("Autor Mínimo 4 Caracteres"),
-    check("descripcion").isLength({ min: 4 }).withMessage("Descripción Mínimo 4 Caracteres"),
-    check("precio").isNumeric({ min: 2 }).withMessage("Precio Inválido"),
-    check("categoria").custom(async (categoria, { req }) => {
-        console.log("categoria", categoria);
-        if ((categoria != "lanzamientos") && (categoria != "seleccionados") && (categoria != "masVendidos")) {
-            throw new Error('Categoría incorrecta')
-        }
-    }),
-    ],
-    productsController.store);
+checkCreateProduct,productsController.store);
 
 /*** EDIT ONE PRODUCT ***/
 router.get("/edit/:id", productsController.edit);
-router.put("/edit/:id", upload.single("product-img"),
-    [
-        check("nombre").isLength({ min: 5 }).withMessage("Nombre Mínimo 5 Caracteres"),
-        check("codigo").isLength({ min: 4 }).withMessage("Código Mínimo 4 Caracteres"),
-        check("autor").isLength({ min: 4 }).withMessage("Autor Mínimo 4 Caracteres"),
-        check("descripcion").isLength({ min: 4 }).withMessage("Autor Mínimo 4 Caracteres"),
-        check("precio").isNumeric({ min: 2 }).withMessage("Precio Inválido"),
-        check("categoria").custom(async (categoria, { req }) => {
-            console.log("categoria", categoria);
-            console.log("categoria", "seleccionados");
-            if ((categoria != "lanzamientos") && (categoria != "seleccionados") && (categoria != "masVendidos")) {
-                throw new Error('Categoría incorrecta')
-            }
-        })
-    ],
-    productsController.update);
+
+router.put("/edit/:id", upload.single("product-img"), 
+checkEditProduct,productsController.update);
 
 /*** DELETE ONE PRODUCT***/
 router.delete('/eliminar/:id', productsController.destroy);
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -7,6 +7,30 @@ const db = require('../database/models');
 const sequelize = db.sequelize;
 const path = require('path');
 
+const checkLoginUser = [
+    check("usuario").isLength({ min: 2 }).withMessage("Usuario Mínimo 2 Caracteres"),
+    check("pass").isLength({ min: 4 }).withMessage("Password Mínimo 4 Caracteres")
+];
+const checkEditUser = [
+    check("usuario").isLength({ min: 4 }).withMessage("Usuario Mínimo 4 Caracteres"),
+    check("email").isEmail().withMessage("Debe ser Email"),
+    check("nacimiento").isDate().withMessage("Debe ser Fecha"),
+
+];
+
+const checkCreateUser = [
+    check("usuario").isLength({ min: 4 }).withMessage("Usuario Mínimo 4 Caracteres"),
+    check("pass").isLength({ min: 4 }).withMessage("Password Mínimo 4 Caracteres"),
+    check("passC").custom(async (passC, { req }) => {
+        const password = req.body.pass;
+        if (password !== passC) {
+            throw new Error('Las Contraseñas deben ser iguales')
+        }
+    }),
+    check("email").isEmail().withMessage("Debe ser Email"),
+    check("nacimiento").isDate().withMessage("Debe ser Fecha")
+];
+
 var storage = multer.diskStorage({
 
     destination: function (req, file, cb) {
@@ -19,14 +43,8 @@ var storage = multer.diskStorage({
 
 var upload = multer({
     storage: storage,
-    //fileFilter: (req, file, cb) => {
-    //  if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-    //      cb(null, true);
-    //   } else {
-    //     cb(null, false);
-    //     return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-    //  }}
 });
+
 // ************ Controller Require ************
 const usersController = require('../controllers/usersController');
 const guestMiddleware = require('../middlewares/guestMiddleware');
@@ -34,38 +52,21 @@ const notUniqueEmailMiddleware = require('../middlewares/notUniqueEmailMiddlewar
 
 /*** CREATE ONE USER ***/
 router.get('/register', usersController.register);
-router.post('/',upload.single("avatar_img"), [
-    check("usuario").isLength({ min: 4 }).withMessage("Usuario Mínimo 4 Caracteres"),
-    check("pass").isLength({ min: 4 }).withMessage("Password Mínimo 4 Caracteres"),
-    check("passC").custom(async (passC, { req }) => {
-        const password = req.body.pass;
-        if (password !== passC) {
-            throw new Error('Las Contraseñas deben ser iguales')
-        }
-    }),
-        check("email").isEmail().withMessage("Debe ser Email"),
-    check("nacimiento").isDate().withMessage("Debe ser Fecha")
-],  notUniqueEmailMiddleware,usersController.create);
+
+router.post('/', upload.single("avatar_img"), checkCreateUser,
+    notUniqueEmailMiddleware, usersController.create);
 
 router.get("/edit/:id", usersController.edit);
 router.put("/edit/:id", upload.single("avatar_img"),
-    [
-        check("usuario").isLength({ min: 4 }).withMessage("Usuario Mínimo 4 Caracteres"),
-        check("email").isEmail().withMessage("Debe ser Email"),
-        check("nacimiento").isDate().withMessage("Debe ser Fecha"),
-
-    ],
-    notUniqueEmailMiddleware,usersController.update);
+    checkEditUser, notUniqueEmailMiddleware, usersController.update);
 router.get('/list', usersController.list);
 router.delete('/eliminar/:id', usersController.destroy);
 router.get('/detalle/:id', usersController.detalle);
 //router.get("/login", guestMiddleware, usersController.login);
 router.get("/login", usersController.login);
 router.get("/logout", usersController.logout);
-router.post("/login", [
-    check("usuario").isLength({ min: 2 }).withMessage("Usuario Mínimo 2 Caracteres"),
-    check("pass").isLength({ min: 4 }).withMessage("Password Mínimo 4 Caracteres")
-],
+
+router.post("/login", checkLoginUser,
     usersController.processLogin);
 router.get("/register", usersController.register
 );
